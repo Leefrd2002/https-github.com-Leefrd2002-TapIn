cd99b27e3d058c95b04b90d109287b1613de5d6a
Todd Lee <Leefrd2002@gmail.com>
2025-07-24 11:43:00 -0500

    Initial Commit

---
+++ TapIn/TapInApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapInApp.swift
+//  TapIn
+//
+//  Created by Todd  Lee on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TapInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
